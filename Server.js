0c26a7b5d1fe1e677bbe52359f30bb05e9a1ec22
Jona2015 <158746834+Jona2015@users.noreply.github.com>
2026-01-28 12:59:10 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,20 +1,36 @@
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 3000 });
 
+const wss = new WebSocket.Server({ port: 3000 });
 const players = {};
 
 wss.on("connection", ws => {
-  const id = Math.random().toString(36).substr(2, 9);
-  players[id] = { x: 0, y: 0 };
+  const id = Math.random().toString(36).slice(2);
+
+  players[id] = {
+    x: 0,
+    y: 0,
+    len: 20
+  };
 
   ws.on("message", msg => {
     const data = JSON.parse(msg);
     players[id] = data;
   });
 
-  ws.on("close", () => delete players[id]);
+  ws.on("close", () => {
+    delete players[id];
+  });
 
-  setInterval(() => {
-    ws.send(JSON.stringify(players));
-  }, 50);
+  ws.send(JSON.stringify({ id }));
 });
+
+setInterval(() => {
+  const data = JSON.stringify(players);
+  wss.clients.forEach(c => {
+    if (c.readyState === WebSocket.OPEN) {
+      c.send(data);
+    }
+  });
+}, 50);
+
+console.log("Server läuft auf Port 3000");
